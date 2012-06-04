64b3ad0ea5067e8cf45dcef842e5fc1faa3c2326
Greg Tomei <greg@handl.it>
2012-06-03 19:16:38 -0700

    ran rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todoko::Application.config.secret_token = '646e1b98aa208fe30cd2aae3129da50c8c698315d2cbed9d3e6631f48be78058531f3d460dc0fa903649bd073e93a2d01a44905e9b9eead4ff885bf0ba6c9327'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Todoko::Application.config.session_store :cookie_store, key: '_todoko_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Todoko::Application.config.session_store :active_record_store
